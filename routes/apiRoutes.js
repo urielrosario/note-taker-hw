580664036a5b6e2f1158aa984d0f6ad7637ab553
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-20 13:07:14 -0400

    apiroutes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,2 +1,20 @@
 const fs = require("fs");
-const { v4: uuid } = require(uuid);
+const uniqid = require("uniqid");
+const database = require("../db/db.json");
+
+module.exports = (app) => {
+  app.get("api/notes", (req, res) => res.json(database));
+
+  app.post("/api/notes", (req, res) => {
+    req.body["id"] = uniqid();
+    database.push(req.body);
+    console.log(database);
+    res.json(database);
+    fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  });
+  app.delete("api/notes/:id", (req, res) => {
+    var deleteNote = database.findIndex((i) => i.id === req.params.id);
+    database.splice(deleteNote);
+    fs.writeFileSync("./db/db.json", JSON.stringify(database));
+  });
+};
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,6 +3,7 @@ const express = require("express");
 
 module.exports = (app) => {
   app.use(express.static("public"));
+
   app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
