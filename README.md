02b9d4bbddc034e8617ae5ff646568fcd204a6df
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-20 17:05:45 -0400

    final

--- README.md
+++ README.md
@@ -41,8 +41,8 @@ THEN I am presented with empty fields to enter a new note title and the note’s
 
 ## Screenshot
 
-![screenshot](images/screenshot.png)
-
+![screenshot](images/Screenshot.png)
+![walkthrough](images/walkthrough.gif)
 
 ```
 WHEN I open the Note Taker
@@ -59,5 +59,7 @@ WHEN I click on the Write icon in the navigation at the top of the page
 THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
 
 ```
+
 ## Heroku Deploy
+
 https://frozen-river-92208.herokuapp.com/
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,18 +3,19 @@ const uniqid = require("uniqid");
 const database = require("../db/db.json");
 
 module.exports = (app) => {
-  app.get("api/notes", (req, res) => res.json(database));
+  app.get("/api/notes", (req, res) => res.json(database));
 
   app.post("/api/notes", (req, res) => {
-    req.body["id"] = uniqid();
+    req.body.id = uniqid();
     database.push(req.body);
-    console.log(database);
-    res.json(database);
+    // console.log(database);
+    res.json(req.body);
     fs.writeFileSync("./db/db.json", JSON.stringify(database));
   });
-  app.delete("api/notes/:id", (req, res) => {
-    var deleteNote = database.findIndex((i) => i.id === req.params.id);
-    database.splice(deleteNote);
+  app.delete("/api/notes/:id", (req, res) => {
+    const deleteNote = (object) => object.id === req.params.id;
+    database.splice(database.findIndex(deleteNote), 1);
     fs.writeFileSync("./db/db.json", JSON.stringify(database));
+    res.json("done");
   });
 };
