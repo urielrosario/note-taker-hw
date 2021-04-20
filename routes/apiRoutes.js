b955f00757d202e91ab5dbd62fb3197dff89bb1c
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-20 12:26:18 -0400

    htmlroute

---
+++ routes/apiRoutes.js
@@ -0,0 +1,2 @@
+const fs = require("fs");
+const { v4: uuid } = require(uuid);
--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require();
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
   console.log(`App listening on port : ${PORT}`);
