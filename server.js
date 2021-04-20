1f585fc4312e0400ec1048cd43d94bf2a64b9a5b
Uriel Rosario <urielrosario1010@gmail.com>
2021-04-20 12:17:40 -0400

    server

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require();
+
+app.listen(PORT, () => {
+  console.log(`App listening on port : ${PORT}`);
+});
